27263c3e379b75e3782ad7cc0668055ce3f8c621
ty-ong-ty <ty_ong_ty@hotmail.com>
2022-07-22 11:57:57 +0800

    database & backend linkage
    - added apis for database linking

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+import db from "./database.js";
 import router from "./router.js";
 import express from "express";
 import cors from "cors";
 
 const app = express();
+
+try {
+  await db.authenticate();
+  console.log("Database connected...");
+} catch (error) {
+  console.error("Connection error:", error);
+}
 
 app.use(cors());
 app.use(express.json());
